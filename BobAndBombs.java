eb530aee9c9d9a5f9296bb8c9fc33bcbba673056
nicole-mathias <47077945+nicole-mathias@users.noreply.github.com>
2021-02-23 18:54:47 +0530

    Update BobAndBombs.java

--- BobAndBombs.java
+++ BobAndBombs.java
@@ -1,3 +1,30 @@
+// Problem
+// Bob and Khatu are brave soldiers in World War 3. They have spotted an enemy troop which is planting bombs. They sent message to the command centre containing characters W and B where W represents a wall and B represents a Bomb. They asked command to tell them how many walls will be destroyed if all bombs explode at once. One bomb can destroy 2 walls on both sides.
+
+// Input:
+
+// First line of input contains number of test cases T. Each test case contains a single string which contains two type of chars 'W' and 'B'.
+
+// Output:
+
+// For each test case print the total number of destroyed wall.
+
+// Constraints:
+
+// 1 ≤ T ≤ 10
+// 1 ≤ |S| ≤ 105
+
+// Sample Input
+// 3
+// WBW
+// WWBWWBW
+// BWWWBWBWW
+// Sample Output
+// 2
+// 5
+// 6
+
+
 import java.util.*;
 public class BobAndBombs{
     public static void main(String args[]){
